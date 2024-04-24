1bda429405cb8649481e14e98e052e0b8ea18c4b
Jancles <154693313+Jancles@users.noreply.github.com>
2024-04-24 16:22:10 +0500

    events.py
    дополнил ивенты

--- Scripts/events.py
+++ Scripts/events.py
@@ -1,13 +1,12 @@
 import pygame
 import random
 import time
-from inventory import *
 Wsize = (1024, 720)
 sc = pygame.display.set_mode(Wsize)
-ooo = inven()
+
 class events :
     count = 0
-    def events():
+    def events_partical():
         self.count = 0
         pass
     def partical_rain(self, count) :
@@ -15,25 +14,16 @@ class events :
         while count != 0 :
             count = count - 1
             pygame.draw.rect(sc , (0,0,255) , [random.randint(0,1000) , random.randint(0,1000) , 5 , 10])
-        
-        ooo.write_hand()
         pygame.display.update()
         return self
         pass
-    def partical_blink(self, timec) :
-        self.timec = timec
-        while timec != 0 :
-            pygame.draw.rect(sc , (0,0,0) , (0 , 0 , 1000 , 1000))
-            timec = timec - 1
+    def partical_thunder():
+        pygame.draw.rect(sc , (0,0,255) , [ 500,  500, 50 , 500])
         pygame.display.update()
-        return self
         pass
-    def partical_blink(self, timec) :
-        self.timec = timec
-        while timec != 0 :
-            pygame.draw.rect(sc , (0,0,0) , (0 , 0 , 1000 , 1000))
-            timec = timec - 1
+    def partical_fire():
+        pygame.draw.rect(sc , (255,0,0) , [500 , 500 , 20 , 20])
         pygame.display.update()
-        return self
         pass
+    
         
