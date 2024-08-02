472258a22f942170df8540e6e585c5c7be8bda3c
Jancles <154693313+Jancles@users.noreply.github.com>
2024-08-02 22:13:03 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import sys
+
 import pygame as pgame
+
 import platform
+
 print(f"""Versios OF:
       Pygame:{pgame.version.ver}
       Python:{sys.version}
-      Machine:{platform.machine()}
-      System:{platform.system()}
-      Version:{platform.version()}
-      Platform:{platform.platform()}
-      Processor:{platform.processor()}
-      """
+      Machine:{platform.machine()
+}  System:{platform.system()
+} Version:{platform.version()
+} Platform:{
+
+platform.platform()
+} Processor:{platform.processor()
+} """
       )
-
-
-
-
 
