47fa83f9fd7a591575c806a448d488888de61a3f
Jancles <154693313+Jancles@users.noreply.github.com>
2024-08-02 21:59:54 +0400

    тз выполнил

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import sys
 import pygame as pgame
-
+import platform
 print(f"""Versios OF:
       Pygame:{pgame.version.ver}
       Python:{sys.version}
       """
       )
+platform.machine()
+platform.system()
+platform.version()
+platform.platform()
+platform.processor()
